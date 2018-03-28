b9bb4a6cf789aab0f4fedc358c703849ec9d0ef1
Tyler Smith <twsmith5280@gmail.com>
2018-03-28 09:38:14 -0600

    added /results route

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -1,18 +1,22 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+// app.set("view engine", "ejs");
 
-
-app.get("/", function(req, res){
-  res.send("Welcome to your Movie Search!");
+app.get('/', function(req, res) {
+  res.send("hi");
+  console.log("welcome to the movie search!");
 });
 
-
-app.get("/results", function(req, res){
-  res.render();
+app.get("/results", function(req, res) {
+  request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var results = JSON.parse(body);
+      res.send(results["Search"][0]["Title"]);
+    }
+  });
 });
 
-
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log("SERVER HAS STARTED!!!");
 });
