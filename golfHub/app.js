ed512563de8faa653d5ea730d5a3c3094fc2b843
Tyler Smith <twsmith5280@gmail.com>
2018-04-01 10:30:04 -0600

    add new course form with body-parser to get data

--- golfHub/app.js
+++ golfHub/app.js
@@ -9,12 +9,14 @@ app.get("/", function(req, res){
   res.render("landing");
 });
 
+var courses = [
+  {name: "Arrowhead Golf Club", image: "http://www.eighteenunderpar.com/wp-content/uploads/2014/09/arrowhead-2.jpg", url: "http://www.arrowheadcolorado.com/"},
+  {name: "Castle Pines Golf Club", image: "http://www.nicklaus.com/design/assets/images/castlepines/gallery/6.jpg", url: "https://www.castlepinesgolfclub.club/"},
+  {name: "Sanctuary Golf Course", image: "http://coloradowomensgolf.org/wp-content/uploads/Sanctuary600-001.jpg", url: "https://sanctuarygolfcourse.com/"},
+];
+
 app.get('/courses', function (req, res) {
-  var courses = [
-    {name: "Arrowhead Golf Club", image: "http://www.eighteenunderpar.com/wp-content/uploads/2014/09/arrowhead-2.jpg", url: "http://www.arrowheadcolorado.com/"},
-    {name: "Castle Pines Golf Club", image: "http://www.nicklaus.com/design/assets/images/castlepines/gallery/6.jpg", url: "https://www.castlepinesgolfclub.club/"},
-    {name: "Sanctuary Golf Course", image: "http://coloradowomensgolf.org/wp-content/uploads/Sanctuary600-001.jpg", url: "https://sanctuarygolfcourse.com/"},
-  ];
+
   res.render("courses", {courses: courses});
 });
 
@@ -23,7 +25,14 @@ app.get('/courses/new', function (req, res) {
 });
 
 app.post('/courses', function (req, res) {
-  res.send("you hit the post route");
+  // get data from form and add to courses array
+  var name = req.body.name;
+  var image = req.body.image;
+  var url = req.body.url;
+  var newCourse = {name: name, image: image, url: url};
+  courses.push(newCourse);
+  // redirect back to courses page
+  res.redirect('/courses');
 });
 
 app.listen(3000, function() {
